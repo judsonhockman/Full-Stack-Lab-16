92d4bebfff1f820ec0187178cb90fc3ce9a8ecb0
Judson Hockman <judson.hockman@gmail.com>
2017-06-28 16:17:10 -0400

    Final...and this time I mean it!

--- client/scripts.js
+++ client/scripts.js
@@ -1,6 +1,7 @@
-var $chirpButton = $('#chirp-button');
+var $chirpButton = $('#chirp-button');  // $ let's developer know this is a JQuery object
 var $chirpField = $('#chirp-field');
 var $chirpList = $('#chirp-list');
+var $userSelector = $('#user-selector');
 
 $chirpField.on('input', function () {
     var isEmpty = $chirpField.val().length === 0;
@@ -8,24 +9,21 @@ $chirpField.on('input', function () {
 });
 $chirpButton.click(postChirp);
 
-function postChirp() {
+function postChirp() {  // object literal
     var chirp = {
         message: $chirpField.val(),
-        user: 'Judson',
-        timestamp: new Date().toISOString()
+        userid: $userSelector.val()
     };
-    $.ajax({
-        method: 'POST',
-        url: '/api/chirps',
-        contentType: 'application/json',
-        data: JSON.stringify(chirp)
+    $.ajax({  // $.ajax is a promise
+        method: 'POST', // what kind of request
+        url: '/api/chirps',  // where is it being sent
+        contentType: 'application/json',  // what format is the data in
+        data: JSON.stringify(chirp) // This is the actual data
     }).then(function (success) {
-
         $chirpField.val('');
         $chirpButton.prop('disabled', true);
         getChirps();
     }, function (err) {
-
         console.log(err);
     });
 }
@@ -61,14 +59,14 @@ function addChirpDiv(chirp) {
     var $message = $('<p></p>');
     var $user = $('<h4></h4>');
     var $timestamp = $('<h5></h5>');
-    var $delButton = $('<button class="delete-button">Delete</button>');
+    var $delButton = $('<button class="delete-button enhanced-button red">Delete</button>');
     $delButton.click(function () {
-        deleteChirp(chirp.ID);
+        deleteChirp(chirp.id);
     });
 
-    $message.text(chirp.Message);
-    $user.text(chirp.User);
-    $timestamp.text(new Date(chirp.Timestamp).toLocaleString());
+    $message.text(chirp.message);
+    $user.text(chirp.user);
+    $timestamp.text(new Date(chirp.timestamp).toLocaleString());
 
     $message.appendTo($chirpDiv);
     $user.appendTo($chirpDiv);
@@ -77,3 +75,18 @@ function addChirpDiv(chirp) {
 
     $chirpDiv.appendTo($chirpList);
 }
+
+function showUsers() {
+    $.ajax({  // $.ajax is a promise which leads to the .then two lines later
+        method: 'GET',
+        url: '/api/users'
+    }).then(function (users) {
+        for (var i = 0; i < users.length; i++) {
+            var $userOption = $('<option value="' + users[i].id + '">' + users[i].name + '</option>'); // .id and .name here are directly from the DB and must therefore match case exactly
+            $userSelector.append($userOption);
+        }
+    }, function (err) {
+        console.log(err);
+    });
+}
+showUsers();
--- server/index.js
+++ server/index.js
@@ -10,9 +10,8 @@ var pool = mysql.createPool({
     password: 'chirperpassword',
     database: 'Chirper'
 });
+
 var clientPath = path.join(__dirname, '../client');
-
-
 
 var app = express();
 app.use(express.static(clientPath));
@@ -20,44 +19,42 @@ app.use(express.static(clientPath));
 app.use(bodyParser.json());  // filters incoming and stores on req.body
 
 app.route('/api/chirps')
-    .get(function (req, res) {
+    .get(function (req, res) {  // incoming request and outgoing response
         rows('GetChirps')
-            .then(function (chirps) {
+            .then(function(chirps) {
                 res.send(chirps);
-            }, function (err) {
-                console.log(err);
-                res.sendStatus(500);
-            });
-
-    }).post(function (req, res) {
-        var newChirp = req.body;
-        row('InsertChirp', [newChirp.message, newChirp.user])
-            .then(function (id) {
-                res.status(201).send(id);
-            }).catch(function (err) {
-                console.log(err);
-                res.sendStatus(500);
-            });
-    });
-app.route('/api/chirps/:id')
-    .get(function (req, res) {
-        row('GetChirp', [req.params.id])
-            .then(function (chirp) {
-                res.send(chirps);
-            }).catch(function (err) {
-                console.log(err);
-                res.sendStatus(500);
-            });
-
-    }).put(function (req, res) {
-        empty('UpdateChirp', [req.params.id, req.body.message])
-            .then(function() {
-                res.sendStatus(204);
             }).catch(function(err) {
                 console.log(err);
                 res.sendStatus(500);
             });
-    }).delete(function(req, res) {
+    }).post(function (req, res) {
+        var newChirp = req.body;
+        row('InsertChirp', [newChirp.message, newChirp.userid])
+            .then(function(id) {
+                res.status(201).send(id);
+            }).catch(function(err) {
+                console.log(err);
+                res.sendStatus(500);
+            });
+    });
+app.route('/api/chirps/:id') // get all chirps
+    .get(function (req, res) {
+        row('GetChirp', [req.params.id])
+            .then(function(chirp) {
+                res.send(chirp);
+            }).catch(function (err) {
+                console.log(err);
+                res.sendStatus(500);
+            });
+    }).put(function (req, res) {
+        empty('UpdateChirp', [req.params.id, req.body.message])
+            .then(function() {
+                res.sendStatus(204);
+            }).catch(function (err) {
+                console.log(err);
+                res.sendStatus(500);
+            });
+    }).delete(function (req, res) {
         empty('DeleteChirp', [req.params.id])
             .then(function() {
                 res.sendStatus(204);
@@ -65,58 +62,25 @@ app.route('/api/chirps/:id')
                 console.log(err);
                 res.sendStatus(500);
             });
-    
     });
+
+app.get('/api/users', function (req, res) { // get one chirp
+    rows('GetUsers') // rows because we want multiple things from the database
+        .then(function (users) {
+            res.send(users);
+        }).catch(function (err) {
+            console.log(err);
+            res.sendStatus(500);
+        });
+});
 
 app.listen(3000);
 
-// function readFile(filePath, encoding) {
-//     return new Promise(function (resolve, reject) {
-//         fs.readFile(filePath, encoding, function (err, data) {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(data);
-//             }
-//         });
-//     });
-// }
-// function writefile(filePath, data) {
-//     return new Promise(function (resolve, reject) {
-//         fs.writeFile(filePath, data, function (err) {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve();
-//             }
-//         });
-//     });
-// }
 
-// function getChirps() {
-//     return new Promise(function (resolve, reject) {
-//         pool.getConnection(function (err, connection) {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 connection.query('CALL GetChirps();', function (err, resultsets) {
-
-//                     connection.release();
-//                     if (err) {
-//                         reject(err);
-
-//                     } else {
-//                         resolve(resultsets[0]);
-//                     }
-//                 });
-//             }
-//         });
-//     });
-// }
 
 function callProcedure(procedureName, args) {
-    return new Promise(function (resolve, reject) {
-        pool.getConnection(function (err, connection) {
+    return new Promise(function (resolve, reject) { // Called executof
+        pool.getConnection(function (err, connection) {  // call-back...error is first parameter. Success is second.
             if (err) {
                 reject(err);
             } else {
@@ -131,8 +95,8 @@ function callProcedure(procedureName, args) {
                     }
                 }
                 var callString = 'CALL ' + procedureName + '(' + placeholders + ');';
-                connection.query(callString, args, function (err, resultsets) {
-                   connection.release();
+                connection.query(callString, args, function(err, resultsets) {
+                    connection.release();
                     if (err) {
                         reject(err);
                     } else {
@@ -143,6 +107,7 @@ function callProcedure(procedureName, args) {
         });
     });
 }
+
 function rows(procedureName, args) {
     return callProcedure(procedureName, args)
         .then(function (resultsets) {
@@ -163,26 +128,4 @@ function empty(procedureName, args) {
 
         });
 }
-       //Instead of: res.end();
-       //use:
-   //   res.send("Hello World"); 
-             // to send text/html
-            // or this...
-   //     res.send({name: "John"});
-           // application/json (and it stringifies it for you!)
-
-         // When using res.send()...
-         // Express will automatically set the response status code to 200
-         // If you want to set a different value, use res.status()
-         //•Can be chained
-        //  res.status(201).end(); // or 
-        //  res.status(200).send("Hello!"); // or
-        //  res.status(404).send("Not found!"); 
-// **** Remember:
-//           res.status(404); Sets the status code to 404, but does not send a response (connection remains open). 
-//                            Requires a .send(...) later.
- //       res.sendStatus(404);  
- //       Sets the status code to 404 and sends NOT FOUND as the response which closes the connection.
-
-
 
